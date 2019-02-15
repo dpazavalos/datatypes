8cbca2127f7092fa83baefd0aaa8822a3febac75
Daniel Avalos <dpazavalos@protonmail.com>
2019-02-14 18:05:59 -0600

    Move to module. Expand separate functions. Add list_in

--- gather_unique/README.md
+++ gather_unique/README.md
@@ -1,63 +1,29 @@
-# Factory Template
+# Gather Unique
 
-Side loaded factory template, used to store and create unique memory ID objects
+Simple, unique list gatherer, with *args for passable blacklist check and optional list_in for 
+automated sorting against blacklist/s
+
+##  Installation
+
+```
+pip install gather_unique
+```
 
 ##  Usage
 
 ```python
-from factorytemplate import build               # pointer to init'd factory object
-from factorytemplate import TypeADocumentation  # Optional documentation for objects    
+from gather_unique import GatherUnique
+gather = GatherUnique()
+head = "Header to display"
 
-type_a_1: TypeADocumentation = build.new_type_a_object()
-type_a_2: TypeADocumentation = build.new_type_a_object()
+# Unique list from stdin, no header
+uniq1 = gather.run()
+
+# Unique list from stdin, with provided blacklist and header
+uniq2 = gather.run(['1', '2', '3', '4', '5'], header=head )
+
+# Unique list from list_in= against provided blacklist
+uniq3 = gather.run(['1', '2', '3', '4', '5'], list_in=['6', '6', '1', '2'])
 ```
-## Features
 
-* Versitility
-    * Dedicated dataclass objects, function classes, and sub types can all be integrated into the
-    template 
-
-* Flexibility
-    * Self contained nature means package can be imported and moved across projects with minimal 
-    issues
-
-* Nonobtrusivity
-    * set and forget, just call build.new_x_obj
-
-## File roles
-
-##### __init__.py
-
-Contains 3 types of classes
-* Importable documentation schematics
-    * Used to import 
-    * TypeADocumentation, TypeBDocumentation
-    
-* Inherit point for specific factories
-    * Add new factories to Obj type inheretance
-    * _Factory(TypeAFactory, TypeBFactory)
-    
-* Single init'd pointer to factory, to be imported
-    * Called by higher class/object in need of unique data objects
-    * factorytemplate.build = _Factory() 
-
-##### _type_x_.py
-
-Object specific files. contains 2 types of classes
-* Object actual, contains all needed functions and variables
-    * At this point, format depends entirely on needed use
-
-* Object specific factory, with any needed setup or config required to prepare object for the 
-'real world'
-    * To be inhereted by __init__._Factory
-    
-##### _z_base_engine.py
-
-* Optional baseline engine for data objects
-    * Self contained, include all functions and documentation. Nothing but _type_x.py should import
-    * Recommend one base_engine per package, either parallel or sub packages
-
-##### _z_documentation
-
-External documentation for specific data objects
-* Separate file, so can be imported by _type_x.py and TypeXDocumentation pointer from __init__.py
+Note that args are considered blacklists by default. Use 'header=' and 'list_in=' to differentiate
--- gather_unique/__init__.py
+++ gather_unique/__init__.py
@@ -1,39 +1,68 @@
-"""Reusable unique string gatherer. Returns unique string values only, with optional"""
+"""Reusable unique item gatherer. Returns unique values with optional list_in and blacklists"""
 
 
-class GatherList:
+class GatherUnique:
+    """
+    Reusable unique item gatherer. Returns unique values with optional list_in and blacklists
+    """
 
     def __init__(self):
-        self.args_dict = {}
+        self._black_lists = []
+        """list of items to verify against"""
+
+        self._gathered = []
+        """sorted and unique items"""
+
+    def _clear_holds(self):
+        """Clear hold attributes"""
+        self._black_lists.clear()
+        self._gathered.clear()
 
     def _not_in_args(self, check):
-        for arg_list in self.args_dict.values():
+        """Check item against black lists"""
+        for arg_list in self._black_lists:
             if check in arg_list:
                 return False
         return True
 
-    def run(self, header: str = '', *args: list) -> list:
-        """Gathers input from user, split by newline. Runs until blank line submitted.
-        Checks input value against itself and optional provided lists in args to ensure unique and
-        allowed values only"""
-
-        self.args_dict.clear()
-        for ndx, arg in enumerate(args):
-            self.args_dict[ndx] = arg
-
-        gathered = []
-
+    def _gather_stdin(self, header: str = '') -> None:
+        """
+        Gathers input from user, split by newline. Runs until blank line submitted.
+        Sorts against blacklists into _gathered
+        """
         print("\n" + header + "\n")
 
         prompt: str = None
         while prompt != '':
             prompt = input('> ').lower().strip()
-            if prompt != '' and prompt not in gathered and self._not_in_args(prompt):
-                gathered.append(prompt)
+            if prompt != '' and prompt not in self._gathered and self._not_in_args(prompt):
+                self._gathered.append(prompt)
 
-        return gathered
+    def _gather_list_in(self, list_in):
+        """
+        Gathers items from given list_in, sorts against blacklists into _gathered
+        """
+        for item in list_in:
+            if item not in self._gathered and self._not_in_args(item):
+                self._gathered.append(item)
 
+    def run(self, *args: list, list_in: list = None, header: str = '',) -> list:
+        """
+        Runs gathering and sorting procedures
+        Calls appropriate _gatherer based on list_in presence
+        Returns unique list _gatherd
+        Checks input value against itself and optional provided lists in args to ensure unique and
+        allowed values only
+        """
 
-gatherer = GatherList().run
-head = "sample words to display"
-print(gatherer(head, ['1', '2', '3', '4', '5'], ['6', '7', '8', '9', '10']))
+        self._clear_holds()
+
+        for arg in args:
+            self._black_lists.append(arg)
+
+        if list_in:
+            self._gather_list_in(list_in)
+        else:
+            self._gather_stdin(header)
+
+        return self._gathered
