60957dc789e7b96cf5e3ca35de02d140e3f1502e
Daniel Avalos <dpazavalos@protonmail.com>
2019-02-14 19:12:02 -0600

    Add Unfreeze function

--- frozentemplate/__init__.py
+++ frozentemplate/__init__.py
@@ -21,6 +21,9 @@ class Frozen:
     def freeze_now(self):
         self._frozen = True
 
+    def unfreeze(self):
+        self._frozen = False
+
     def __setattr__(self, item, value):
         """Pre 3.7 emulation of frozen dataclasses. Soft mutation prevention"""
         if self._frozen:
