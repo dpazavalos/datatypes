f5ffd0722a8e6fa61c1d8fefdee35b6d3668dd1f
Daniel Avalos <dpazavalos@protonmail.com>
2019-03-02 22:11:04 -0600

    Expand attributes, blacklisting, use kwargs for better blacklist positioning in run

--- gatherunique/README.md
+++ gatherunique/README.md
@@ -1,7 +1,8 @@
 # Gather Unique
 
-Simple, unique list gatherer, with *args for passable blacklist check and optional list_in for 
-automated sorting against blacklist/s
+Gathers a list of unique entries from user, with optional blacklists and header prompt
+Note: Item type depends on gathering method. If using stdin, anticipate string returns.
+Otherwise, use list_in
 
 ##  Installation
 
@@ -12,18 +13,21 @@ pip install gather_unique
 ##  Usage
 
 ```python
-from gatherunique import GatherUnique
-gather = GatherUnique()
+from gatherunique import GatherUnique as gun
+gather = gun()
 head = "Header to display"
 
 # Unique list from stdin, no header
 uniq1 = gather.run()
 
 # Unique list from stdin, with provided blacklist and header
-uniq2 = gather.run(['1', '2', '3', '4', '5'], header=head )
+uniq2 = gather.run(list_in=['1', '2', '3', '4', '5'], header=head )
 
 # Unique list from list_in= against provided blacklist
-uniq3 = gather.run(['1', '2', '3', '4', '5'], list_in=['6', '6', '1', '2'])
+uniq3 = gather.run(list_in=['6', '6', '1', '2'], 
+                   blacklist0=['1', '2', '3', '4', '5'], 
+                   blacklist1=['7', '8'])
 ```
 
-Note that args are considered blacklists by default. Use 'header=' and 'list_in=' to differentiate
+Note: kwargs used only for flexible positioning. Your blacklist arg names literally do not matter, 
+just differentiate them from the default args
--- gatherunique/__init__.py
+++ gatherunique/__init__.py
@@ -7,30 +7,40 @@ class GatherUnique:
     """
 
     def __init__(self):
-        self._black_lists = []
-        """list of items to verify against"""
 
+        self._header = ''
+        """Optional header to display to user when gathering entries stdin"""
+        self._list_in = []
+        """optional input parameter, in lieu of stdin prompting"""
         self._gathered = []
-        """sorted and unique items"""
+        """Unique items reconciled against potential blacklist(s)"""
+        self._black_lists = []
+        """Optional list(s) of items to verify against"""
 
-    def _clear_holds(self):
-        """Clear hold attributes"""
+    def _reset(self):
+        """
+        Reset attributes
+        """
+        self._header = ''
+        self._list_in.clear()
         self._black_lists.clear()
         self._gathered.clear()
 
-    def _not_in_args(self, check):
-        """Check item against black lists"""
-        for arg_list in self._black_lists:
-            if check in arg_list:
+    def _not_in_args(self, item) -> bool:
+        """
+        Check given item against black lists
+        """
+        for black_list in self._black_lists:
+            if item in black_list:
                 return False
         return True
 
-    def _gather_stdin(self, header: str = '') -> None:
+    def _gather_from_stdin(self) -> None:
         """
         Gathers input from user, split by newline. Runs until blank line submitted.
         Sorts against blacklists into _gathered
         """
-        print("\n" + header + "\n")
+        print("\n" + self._header + "\n")
 
         prompt: str = None
         while prompt != '':
@@ -38,31 +48,54 @@ class GatherUnique:
             if prompt != '' and prompt not in self._gathered and self._not_in_args(prompt):
                 self._gathered.append(prompt)
 
-    def _gather_list_in(self, list_in):
+    def _gather_from_list_in(self):
         """
         Gathers items from given list_in, sorts against blacklists into _gathered
         """
-        for item in list_in:
+        for item in self._list_in:
             if item not in self._gathered and self._not_in_args(item):
                 self._gathered.append(item)
 
-    def run(self, *args: list, list_in: list = None, header: str = '',) -> list:
+    def _return_gathered(self) -> list:
         """
-        Runs gathering and sorting procedures
-        Calls appropriate _gatherer based on list_in presence
-        Returns unique list _gatherd
-        Checks input value against itself and optional provided lists in args to ensure unique and
-        allowed values only
+        Returns gathered list, resets working attributes
+        """
+        # Save gathered, clear attributes, return
+        to_return = self._gathered.copy()
+        self._reset()
+        return to_return
+
+    def run(self,
+            header: str = '', list_in: list = None,
+            **blacklists) -> list:
+        """
+        Gathers a list of unique entries from user, with optional blacklists and header prompt
+        Note: Item type depends on gathering method. If using stdin, anticipate string returns.
+        Otherwise, use list_in
+
+        Args:
+            header: Header prompt to display to user on startup.
+
+            list_in: An optional input parameter. Feeding a list bypasses stdin, and straight to
+                unique list generation. If black lists are provided, reconciles against them.
+
+            blacklists: Optional *kwargs for one or more blacklists to check entires against.
+                Returned list will not contain any blacklisted entries. (Note: kwargs used only for
+                flexible positioning. Arg names literally do not matter)
+
+        Returns:
+            Unique list of values, either from stdin or list_in arg
         """
 
-        self._clear_holds()
-
-        for arg in args:
-            self._black_lists.append(arg)
+        # Assign running attributes
+        self._header = header
+        self._list_in = list_in
+        for blist in blacklists.values():
+            self._black_lists.append(blist)
 
         if list_in:
-            self._gather_list_in(list_in)
+            self._gather_from_list_in()
         else:
-            self._gather_stdin(header)
+            self._gather_from_stdin()
 
-        return self._gathered
+        return self._return_gathered()
